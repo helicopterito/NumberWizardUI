6c876a2db33dca8297d72a4152aaa43d420c60a5
Néstor A. M <alvarado.nestor@yandex.com>
2017-06-14 12:07:58 -0500

    Construida la versión web

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+	private int MAX;
+	private int MIN;
+	private int guess;
+	public int MAX_GUESSES_ALLOWED = 5;
+
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+		StartGame ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void GuessHigher(){
+		MIN = guess;
+		NextGuess ();
+	}
+
+	public void GuessLower(){
+		MAX = guess;
+		NextGuess ();
+	}
+
+	void StartGame(){
+		MAX = 1000;
+		MIN = 0;
+		NextGuess ();
+	}
+
+	void NextGuess(){
+		guess = Random.Range(MIN, MAX+1);
+		text.text = guess.ToString();
+		MAX_GUESSES_ALLOWED--;
+
+		if (MAX_GUESSES_ALLOWED <= 0) {
+			SceneManager.LoadScene ("Win");
+		}
+	}
+}
